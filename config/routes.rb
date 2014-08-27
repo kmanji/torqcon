82dcb07b4423d05a44f1a70b1e781071f9d4792d
kmanji <inkspand@gmail.com>
2014-08-26 22:25:18 -0400

    routes, styling home and app page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'public#home'
-  get 'public/new'
+  get 'public/structural'
 
   
   # The priority is based upon order of creation: first created -> highest priority.
